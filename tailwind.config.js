dbaa5cc4dad97fc1c4643d64ea0d9eb514629a79
emmanuel <emmanuelalabi066@gmail.com>
2026-07-18 14:04:40 +0100

    updated font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],        /* Overrides default sans-serif with Inter */
         tech: ['Space Grotesk', 'sans-serif'], /* Adds a custom class 'font-tech' */
+        bebas: ['"Bebas Neue"', 'sans-serif'],
       },
     },
   },
